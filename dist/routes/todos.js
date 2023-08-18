4be6b7c1122f8248c1f4baa18f283f4445f60d00
Ahmed Khaled <77169294+AhmedKhaleda998@users.noreply.github.com>
2023-08-18 17:43:58 +0300

    Code Isolation

--- dist/routes/todos.js
+++ dist/routes/todos.js
@@ -7,9 +7,10 @@ router.get('/', (req, res, next) => {
     res.status(200).json({ todos: todos });
 });
 router.post('/todo', (req, res, next) => {
+    const body = req.body;
     const newTodo = {
         id: new Date().toISOString(),
-        text: req.body.text,
+        text: body.text,
     };
     todos.push(newTodo);
     res.status(201).json({
@@ -19,14 +20,16 @@ router.post('/todo', (req, res, next) => {
     });
 });
 router.put('/todo/:todoId', (req, res, next) => {
-    const todoId = req.params.todoId;
+    const params = req.params;
+    const todoId = params.todoId;
+    const body = req.body;
     const todoIndex = todos.findIndex(todoItem => todoItem.id === todoId);
     if (todoIndex >= 0) {
         todos[todoIndex] = {
             id: todos[todoIndex].id,
-            text: req.body.text,
+            text: body.text,
         };
-        return res.status(204).json({
+        return res.status(201).json({
             message: 'Updated todo',
             todos: todos
         });
@@ -34,7 +37,8 @@ router.put('/todo/:todoId', (req, res, next) => {
     res.status(404).json({ message: 'Could not find todo for this id.' });
 });
 router.delete('/todo/:todoId', (req, res, next) => {
-    const todoId = req.params.todoId;
+    const params = req.params;
+    const todoId = params.todoId;
     const todoIndex = todos.findIndex(todoItem => todoItem.id === todoId);
     if (todoIndex >= 0) {
         todos.splice(todoIndex, 1);
