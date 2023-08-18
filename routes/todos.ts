d7b139601490b2c039fa5db2fc0616651718f7e2
Ahmed Khaled <77169294+AhmedKhaleda998@users.noreply.github.com>
2023-08-18 17:38:32 +0300

    Use Alias

--- routes/todos.ts
+++ routes/todos.ts
@@ -2,6 +2,9 @@ import { Router } from 'express';
 
 import { Todo } from '../models/todo';
 
+type RequestBody = { text: string };
+type RequestParams = { todoId: string };
+
 let todos: Todo[] = [];
 
 const router = Router();
@@ -11,9 +14,10 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/todo', (req, res, next) => {
+    const body = req.body as RequestBody;
     const newTodo: Todo = {
         id: new Date().toISOString(),
-        text: req.body.text,
+        text: body.text,
     };
     todos.push(newTodo);
     res.status(201).json({
@@ -24,12 +28,14 @@ router.post('/todo', (req, res, next) => {
 });
 
 router.put('/todo/:todoId', (req, res, next) => {
-    const todoId = req.params.todoId;
+    const params = req.params as RequestParams;
+    const todoId = params.todoId;
+    const body = req.body as RequestBody;
     const todoIndex = todos.findIndex(todoItem => todoItem.id === todoId);
     if (todoIndex >= 0) {
         todos[todoIndex] = {
             id: todos[todoIndex].id,
-            text: req.body.text,
+            text: body.text,
         };
         return res.status(201).json({
             message: 'Updated todo',
@@ -40,7 +46,8 @@ router.put('/todo/:todoId', (req, res, next) => {
 });
 
 router.delete('/todo/:todoId', (req, res, next) => {
-    const todoId = req.params.todoId;
+    const params = req.params as RequestParams;
+    const todoId = params.todoId;
     const todoIndex = todos.findIndex(todoItem => todoItem.id === todoId);
     if (todoIndex >= 0) {
         todos.splice(todoIndex, 1);
